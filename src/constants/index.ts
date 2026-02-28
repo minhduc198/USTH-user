7aa5d61eb2a85c0f3453a156a3c5157051ccf2d9
Nguyen Minh Duc <nguyenminhduc1982005@gmail.com>
2026-02-28 14:04:55 +0700

    fix build errors

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-const ipConfig = "192.168.78.100";
+// const ipConfig = "192.168.78.100";
 // export const BASE_URL = `http://${ipConfig}:4000/api/auth`;
 export const BASE_URL = `https://be-usth-user.onrender.com/api/auth`;
